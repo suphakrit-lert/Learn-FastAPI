c2295267b1360acc9d7089d4a484349a1d7e5db8
Suphakrit Lertkitcharoenvong <suphakrit.bcc@hotmail.com>
2025-10-19 16:05:18 -0400

    feat: Query Parameters lesson

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 app = FastAPI()
 
 
@@ -22,9 +24,9 @@ async def read_user_me():
     return {"user_id": "the current user"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 
 @app.get("/models/{model_name}")
@@ -47,3 +49,39 @@ async def read_file(file_path: str):
     http://127.0.0.1:8000/files//home/johndoe/myfile.txt
     """
     return {"file_path": file_path}
+
+    # @app.get("/items/")
+    # async def read_item(skip: int = 0, limit: int = 10):
+    #     """
+    #     Query version
+    #     http://127.0.0.1:8000/items/
+    #     http://127.0.0.1:8000/items/?skip=0&limit=10
+    #     """
+    #     return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    """
+    >>> http://127.0.0.1:8000/items/foo-item?needy=sooooneedy
+    {"item_id":"foo-item","needy":"sooooneedy","skip":0,"limit":null}
+    """
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    """
+    http://127.0.0.1:8000/items/foo?short=True
+    """
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
